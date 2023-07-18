d6e0bad60816f7ea1f65a30aadf297f11372e647
Jensen Lawless <jensenlawless@gmail.com>
2023-07-18 15:20:08 -0600

    changes

--- client/src/components/CoursesHero.jsx
+++ client/src/components/CoursesHero.jsx
@@ -17,16 +17,14 @@ function CoursesHero() {
   return (
     <>
       <div class="grid grid-cols-2">
-        <div class="pl-24 pt-8">Course1</div>
         <div class="grid grid-cols-2">
           {courses.map((course) => (
             <div key={course.id} className="pl-24 pt-8">
-              <h1>{course.name}</h1></div>
+              <h1>{course.name}</h1>
+              <h2>{course.description}</h2>
+            </div>
           ))}
         </div>
-        <div class="pl-24 pt-8">Course2</div>
-        <div class="pl-24 pt-8">Course3</div>
-        <div class="pl-24 pt-8 grid">Course4</div>
       </div>
     </>
   );
